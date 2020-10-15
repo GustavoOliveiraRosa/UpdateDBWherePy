685bc203bf4c025103fd4a2cdbe093f33f2f7a33
Gustavo <gustavoolrosa2019@gmail.com>
2020-10-15 14:06:32 -0300

    Script OK

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
-import requeriments.connector
+from requeriments.connector import *
 
+initialID = int(input("Id Inicial => "))
+finalID = int(input("Id Final => "))
+table = input("Table =>")
+column = input("Coluna =>")
+value = input("Valor =>")
+
+while True:
+    if initialID <= finalID:
+
+        print(str("UPDATE ")+str(table)+str(" SET ")+str(column)+str("='")+str(value)+str("' where id=")+str(initialID))
+        
+
+        initialID = initialID + 1
+
+    else:
+        break
